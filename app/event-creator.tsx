421181fe52b8321ee5534f24e41590453069948f
Luminita Guzovatii <luminita.guzovatii@gmail.com>
2023-08-08 17:56:22 +0300

    adding input validation

--- app/event-creator.tsx
+++ app/event-creator.tsx
@@ -1,14 +1,63 @@
 'use client'
 
-import React from "react";
+import React, { useState } from "react";
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
 export default function EventCreator() {
+
+   
 
     const [name, setName] = React.useState("");
     const [date, setDate] = React.useState("");
     const [calendarId, setCalendarId] = React.useState("");
 
+
+    function validateInput() {
+
+        var valid = true;
+
+        if (name.length == 0) {
+            const el = document.getElementById("name");
+            el.style.border = "2px solid red";
+
+            valid = false;
+            el.onchange = () => {el.style.border = "1px solid grey"};
+        }
+
+        if (date.length == 0) {
+            const el = document.getElementById("date");
+            el.style.border = "2px solid red";
+
+            valid = false;
+            el.onclick = () => { el.style.border = "1px solid grey" };
+        }
+
+        if (calendarId.length == 0) {
+            const el = document.getElementById("calendar");
+            el.style.border = "2px solid red";
+
+            valid = false;
+            el.onchange = () => { el.style.border = "1px solid grey" };
+        }
+
+        if (!valid) {
+
+            alert('Invalid Input')
+            return;
+        }
+
+
+        if (valid)
+            handleClick();
+
+
+
+    }
+
     function handleClick() {
+
+        console.log("a mers");
         let endpoint = 'http://localhost:8080/event';
 
         fetch(endpoint, {
@@ -21,17 +70,23 @@ export default function EventCreator() {
     }
 
     return (
-        <>
+        <form autoComplete="off">
+            <div>
             <label>Name: </label>
-            <input type="text" value={name} onChange={e => { setName(e.currentTarget.value); }}></input>
-            <br/>
+                <input id="name" type="text" value={name} onChange={e => { setName(e.currentTarget.value); }}></input>
+            </div>
+            <br />
+            <div>
             <label>Date: </label>
-            <input type="text" value={date} onChange={e => { setDate(e.currentTarget.value); }}></input>
-            <br/>
+                <DatePicker id = "date" selected={date} onChange={(date) => setDate(date)} />
+            </div>
+            <br />
+            <div>
             <label>Calendar: </label>
-            <input type="text" value={calendarId} onChange={e => { setCalendarId(e.currentTarget.value); }}></input>
+                <input type="text" id="calendar" value={calendarId} onChange={e => { setCalendarId(e.currentTarget.value); }}></input>
+            </div>
             <br/>
-            <button type="button" onClick={handleClick}>Create</button>
-        </>
+            <button type="button" onClick={validateInput}>Create</button>
+        </form>
     )
 }
