1e939b2f2947f18f7fe51ec7d37fa67f3c3db77a
Luminita Guzovatii <luminita.guzovatii@gmail.com>
2023-08-17 13:26:14 +0300

    revert all unnecessary changes

--- app/page.tsx
+++ app/page.tsx
@@ -6,80 +6,80 @@ import EventCreator from './event-creator';
 export const dynamic = 'force-dynamic'
 
 export default async function Page() {
-    let now = new Date();
+  let now = new Date();
 
-    let startDate = new Date(now);
-    startDate.setMonth(startDate.getMonth() - 6)
+  let startDate = new Date(now);
+  startDate.setMonth(startDate.getMonth() - 6)
+  
+  let endDate = new Date(now);
+  endDate.setMonth(endDate.getMonth() + 6)
 
-    let endDate = new Date(now);
-    endDate.setMonth(endDate.getMonth() + 6)
+  const calendar = await getCalendar()
+  console.log(calendar)
+  for(var event of calendar.events) {
+    event.date_time = new Date(event.date_time);
+  }
 
-    const calendar = await getCalendar()
-    console.log(calendar)
-    for (var event of calendar.events) {
-        event.date_time = new Date(event.date_time);
+  let values = [];
+  for (var d = new Date(startDate); d <= endDate; d.setDate(d.getDate() + 1)) {
+    let count = 0;
+    
+    for(var event of calendar.events) {
+      if (event.date_time.getUTCFullYear() === d.getUTCFullYear() && event.date_time.getUTCMonth() === d.getUTCMonth() && event.date_time.getUTCDate() === d.getUTCDate()) {
+        count=count+1;
+      }
     }
 
-    let values = [];
-    for (var d = new Date(startDate); d <= endDate; d.setDate(d.getDate() + 1)) {
-        let count = 0;
+    values.push({date: new Date(d), count: count});
+  }
 
-        for (var event of calendar.events) {
-            if (event.date_time.getUTCFullYear() === d.getUTCFullYear() && event.date_time.getUTCMonth() === d.getUTCMonth() && event.date_time.getUTCDate() === d.getUTCDate()) {
-                count = count + 1;
-            }
-        }
+  let eventsByDate = new Map<string, string[]>();
+  for(var event of calendar.events) {
+    // let key = event.date_time.getUTCFullYear().toString().concat('-').concat(event.date_time.getUTCMonth().toString()).concat('-').concat(event.date_time.getUTCDate().toString());
+    let key = event.date_time.toISOString().slice(0, 10)
 
-        values.push({ date: new Date(d), count: count });
+    if(!eventsByDate.has(key)) {
+      eventsByDate.set(key, []);
     }
 
-    let eventsByDate = new Map<string, string[]>();
-    for (var event of calendar.events) {
-        // let key = event.date_time.getUTCFullYear().toString().concat('-').concat(event.date_time.getUTCMonth().toString()).concat('-').concat(event.date_time.getUTCDate().toString());
-        let key = event.date_time.toISOString().slice(0, 10)
+    eventsByDate.get(key).push(event.name);
+  }
 
-        if (!eventsByDate.has(key)) {
-            eventsByDate.set(key, []);
-        }
-
-        eventsByDate.get(key).push(event.name);
-    }
-
-    return (
-        <>
-            <HeatMap
-                startDate={startDate}
-                endDate={endDate}
-                values={values}
-                eventsByDate={eventsByDate}
-            />
-            <EventCreator />
-        </>
-    )
+  return (
+    <>
+      <HeatMap
+        startDate={startDate}
+        endDate={endDate}
+        values={values}
+        eventsByDate={eventsByDate}
+      />
+      <EventCreator/>
+    </>
+  )
 }
 
 async function getCalendar(): Promise<Calendar> {
-    try {
-        const res = await fetch('http://127.0.0.1:8080/calendar/aguzovatii', { cache: 'no-store' })
+  try {
+    const res = await fetch('http://127.0.0.1:8080/calendar/aguzovatii', {cache: 'no-store'})
 
-        if (!res.ok) {
-            throw new Error('Failed to fetch data')
-        }
-
-        return res.json()
-    } catch (err) {
-        return new Promise((resolve, reject) => { resolve({ id: 'aguzovatii', events: [] }); });
+    if(!res.ok){
+      throw new Error('Failed to fetch data')
     }
+
+    return res.json()
+  } catch (err) {
+    return new Promise((resolve, reject) => { resolve({id: 'aguzovatii', events: []}); });
+  }
 }
 
 interface Calendar {
-    id: string;
-    events: Event[];
+  id: string;
+  events: Event[];
 
 }
 
 interface Event {
-    date_time: Date;
-    id: string;
-    name: string;
+date_time: Date;
+id: string;
+name: string;
 }
