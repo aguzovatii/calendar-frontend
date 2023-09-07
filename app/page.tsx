ae92f9bfd00815e37fd57e4ff4b38ec2f8291536
DianaJacota <48437464+DianaJacota@users.noreply.github.com>
2023-09-07 11:17:39 +0300

    Implement basic user signup functionality (#8)
    * Implement basic user signup functionality
    
    - move the logic that shows the calendar in a separate folder
      (calendar-heatmap, event-creator and the newly created file,
      calendar-page)
    - pass the username parameter to the /event and /calendar/{username}
      endpoints

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,21 @@
-'use client'
-import HeatMap from './calendar-heatmap';
-import EventCreator from './event-creator';
-import useSWR from 'swr';
-
-const fetcher = (url) => fetch(url).then((res) => res.json());
+"use client";
+import { useState } from "react";
+import CalendarPage from "./calendar/calendar-page";
+import Signup from "./user/user-creator";
 
 export default function Page() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-  const now = new Date();
-
-  let startDate = new Date(now);
-  startDate.setMonth(startDate.getMonth() - 6)
-
-  let endDate = new Date(now);
-  endDate.setMonth(endDate.getMonth() + 6)
-
-  const { data, error, isLoading, mutate } = useSWR(process.env.NEXT_PUBLIC_CALENDAR_BACKEND_URL + '/calendar/aguzovatii', fetcher)
-
-  if (error) return <div>failed to load</div>
-  if (isLoading) return <div>loading...</div>
+  if (username.length > 0) {
+    return <CalendarPage username={username} />;
+  }
 
   return (
-    <>
-      <HeatMap 
-        startDate={startDate}
-        endDate={endDate}
-        events={data.events}
-      />
-      <EventCreator onEventCreated={() => mutate()} />
-    </>
-  )
+    <Signup
+      onSignup={(username: string, password: string) => {
+        setUsername(username), setPassword(password);
+      }}
+    />
+  );
 }
---
+++ app/user/user-creator.tsx
@@ -0,0 +1,82 @@
+import { useState } from "react";
+
+export default function Signup({ onSignup }) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <>
+      <h1>Signup</h1>
+      <form autoComplete="off">
+        {" "}
+        <div>
+          {" "}
+          <label>Username: </label>{" "}
+          <input
+            id="username"
+            type="text"
+            value={username}
+            onChange={(e) => {
+              setUsername(e.currentTarget.value);
+            }}
+          ></input>{" "}
+        </div>{" "}
+        <br />{" "}
+        <div>
+          {" "}
+          <label>Password: </label>{" "}
+          <input
+            id="password"
+            type="password"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.currentTarget.value);
+            }}
+          ></input>{" "}
+        </div>{" "}
+        <br />{" "}
+        <button type="button" onClick={validateInput}>
+          {" "}
+          Create{" "}
+        </button>{" "}
+      </form>
+    </>
+  );
+
+  function validateInput() {
+    var valid = true;
+    var errstyle = "2px solid red";
+    var initstyle = "1px solid grey";
+
+    if (username.length === 0) {
+      const el = document.getElementById("username");
+      el.style.border = errstyle;
+      valid = false;
+      el.onchange = () => {
+        (el.style.border = initstyle), (username) => setUsername(username);
+      };
+    }
+
+    if (password.length === 0) {
+      const el = document.getElementById("password");
+      el.style.border = errstyle;
+      valid = false;
+      el.onchange = () => {
+        (el.style.border = initstyle), (password) => setPassword(password);
+      };
+    }
+    valid ? handleClick() : alert("Invalid Input");
+  }
+
+  function handleClick() {
+    fetch(process.env.NEXT_PUBLIC_CALENDAR_BACKEND_URL + '/user', {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ username, password }),
+    }).then((response) => {
+      response.ok
+        ? onSignup(username, password)
+        : alert("There was an error. Please try again later");
+    });
+  }
+}
