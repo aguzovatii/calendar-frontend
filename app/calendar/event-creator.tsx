7ff145b8d3181adb3d2cb9d6134e1944bbcccec4
DianaJacota <48437464+DianaJacota@users.noreply.github.com>
2023-09-07 10:09:28 +0300

    Update app/calendar/event-creator.tsx
    use process.env.NEXT_PUBLIC_CALENDAR_BACKEND_URL to get the backend address
    
    Co-authored-by: aguzovatii <guzovatii.anatolii@gmail.com>

--- app/calendar/event-creator.tsx
+++ app/calendar/event-creator.tsx
@@ -44,7 +44,7 @@ export default function EventCreator({ username, onEventCreated }) {
   }
 
   function handleClick() {
-    fetch("http://localhost:8080/event", {
+    fetch(process.env.NEXT_PUBLIC_CALENDAR_BACKEND_URL + '/event', {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
