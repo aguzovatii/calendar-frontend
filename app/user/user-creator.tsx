9975085b50ab0aaf87386045bb4da8da536f3529
DianaJacota <48437464+DianaJacota@users.noreply.github.com>
2023-09-08 20:10:56 +0300

    Implement user login basic functionality (#10)
    - add a new form for the Login action in the same page with the Signup
      form

--- app/user/user-creator.tsx
+++ app/user/user-creator.tsx
@@ -4,14 +4,49 @@ export default function Signup({ onSignup }) {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
+  const [usernameLogin, setUsernameLogin] = useState("");
+  const [passwordLogin, setPasswordLogin] = useState("");
+
   return (
     <>
+      <h1>Login</h1>
+      <form autoComplete="off">
+        <div>
+          <label>Username: </label>
+          <input
+            id="usernameLogin"
+            type="text"
+            value={usernameLogin}
+            onChange={(e) => {
+              setUsernameLogin(e.currentTarget.value);
+            }}
+          ></input>
+        </div>
+        <br />
+        <div>
+          <label>Password: </label>
+          <input
+            id="passwordLogin"
+            type="password"
+            value={passwordLogin}
+            onChange={(e) => {
+              setPasswordLogin(e.currentTarget.value);
+            }}
+          ></input>
+        </div>
+        <br />
+        <button type="button" onClick={validateInputLogin}>
+          Login
+        </button>
+      </form>
+
+      <br />
+      <br />
+
       <h1>Signup</h1>
       <form autoComplete="off">
-        {" "}
         <div>
-          {" "}
-          <label>Username: </label>{" "}
+          <label>Username: </label>
           <input
             id="username"
             type="text"
@@ -19,12 +54,11 @@ export default function Signup({ onSignup }) {
             onChange={(e) => {
               setUsername(e.currentTarget.value);
             }}
-          ></input>{" "}
-        </div>{" "}
-        <br />{" "}
+          ></input>
+        </div>
+        <br />
         <div>
-          {" "}
-          <label>Password: </label>{" "}
+          <label>Password: </label>
           <input
             id="password"
             type="password"
@@ -32,14 +66,13 @@ export default function Signup({ onSignup }) {
             onChange={(e) => {
               setPassword(e.currentTarget.value);
             }}
-          ></input>{" "}
-        </div>{" "}
-        <br />{" "}
+          ></input>
+        </div>
+        <br />
         <button type="button" onClick={validateInput}>
-          {" "}
-          Create{" "}
-        </button>{" "}
-      </form>
+          Create
+        </button>
+      </form> 
     </>
   );
 
@@ -68,6 +101,31 @@ export default function Signup({ onSignup }) {
     valid ? handleClick() : alert("Invalid Input");
   }
 
+  function validateInputLogin() {
+    var valid = true;
+    var errstyle = "2px solid red";
+    var initstyle = "1px solid grey";
+
+    if (usernameLogin.length === 0) {
+      const el = document.getElementById("usernameLogin");
+      el.style.border = errstyle;
+      valid = false;
+      el.onchange = () => {
+        (el.style.border = initstyle), (usernameLogin) => setUsernameLogin(usernameLogin);
+      };
+    }
+
+    if (passwordLogin.length === 0) {
+      const el = document.getElementById("passwordLogin");
+      el.style.border = errstyle;
+      valid = false;
+      el.onchange = () => {
+        (el.style.border = initstyle), (passwordLogin) => setPasswordLogin(passwordLogin);
+      };
+    }
+    valid ? handleClickLogin() : alert("Invalid Input");
+  }
+
   function handleClick() {
     fetch(process.env.NEXT_PUBLIC_CALENDAR_BACKEND_URL + '/user', {
       method: "POST",
@@ -76,6 +134,18 @@ export default function Signup({ onSignup }) {
     }).then((response) => {
       response.ok
         ? onSignup(username, password)
+        : alert("There was an error. Please try again later");
+    });
+  }
+
+  function handleClickLogin() {
+    fetch(process.env.NEXT_PUBLIC_CALENDAR_BACKEND_URL + '/login', {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ username : usernameLogin, password : passwordLogin }),
+    }).then((response) => {
+      response.ok
+        ? onSignup(usernameLogin, passwordLogin)
         : alert("There was an error. Please try again later");
     });
   }
