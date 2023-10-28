4374641c0a8f8476622cfb76e13e064f0ab03466
Anatolii Guzovatii <guzovatii.anatolii@gmail.com>
2023-10-28 21:28:25 +0300

    Move from vanilla CSS to tailwind

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -35,47 +35,86 @@ export default function Signin() {
   const error = errorType && (errors[errorType] ?? errors.default);
 
   return (
-    <div className="page">
-      <div className="signin">
-        <div className="card">
-          <h1>Sign in</h1>
-          {error && (
-            <div className="error">
-              <p>{error}</p>
-            </div>
-          )}
-          <form autoComplete="off">
-            <div>
-              <label>Username</label>
+    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+          Sign in
+        </h2>
+      </div>
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        {error && (
+          <div>
+            <p>{error}</p>
+          </div>
+        )}
+        <form className="space-y-6" autoComplete="off">
+          <div>
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Username
+            </label>
+            <div className="mt-2">
               <input
                 id="username"
+                name="username"
                 type="text"
+                required
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 value={username}
                 onChange={(e) => {
                   setUsername(e.currentTarget.value);
                 }}
-              ></input>
+              />
             </div>
-            <br />
-            <div>
-              <label>Password</label>
+          </div>
+
+          <div>
+            <div className="flex items-center justify-between">
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Password
+              </label>
+            </div>
+            <div className="mt-2">
               <input
                 id="password"
+                name="password"
                 type="password"
+                autoComplete="current-password"
+                required
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 value={password}
                 onChange={(e) => {
                   setPassword(e.currentTarget.value);
                 }}
-              ></input>
+              />
             </div>
-            <br />
-            <button type="button" onClick={validateInput}>
+          </div>
+
+          <div>
+            <button
+              type="button"
+              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              onClick={validateInput}
+            >
               Sign in
             </button>
-          </form>
-          <br />
-          <Link href="/auth/signup">Sign up</Link>
-        </div>
+          </div>
+        </form>
+
+        <p className="mt-10 text-center text-sm text-gray-500">
+          Not a member?{" "}
+          <Link
+            className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+            href="/auth/signup"
+          >
+            Sign up
+          </Link>
+        </p>
       </div>
     </div>
   );
