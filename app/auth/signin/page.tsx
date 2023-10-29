e8c94431cf6e514553c9e9f67cae13e304ece7e3
Anatolii Guzovatii <guzovatii.anatolii@gmail.com>
2023-10-29 12:46:40 +0200

    Change form elements color to slate

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -9,7 +9,7 @@ import { redirect } from "next/navigation";
 type SignInErrorTypes = "CredentialsSignin" | "default";
 
 const errors: Record<SignInErrorTypes, string> = {
-  CredentialsSignin: "Sign in failed. Wrong username and/or password.",
+  CredentialsSignin: "Invalid credentials",
   default: "Unable to sign in. Please try again later.",
 };
 
@@ -42,11 +42,6 @@ export default function Signin() {
         </h2>
       </div>
       <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-        {error && (
-          <div>
-            <p>{error}</p>
-          </div>
-        )}
         <form className="space-y-6" autoComplete="off">
           <div>
             <label
@@ -61,7 +56,7 @@ export default function Signin() {
                 name="username"
                 type="text"
                 required
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-slate-600 sm:text-sm sm:leading-6"
                 value={username}
                 onChange={(e) => {
                   setUsername(e.currentTarget.value);
@@ -86,7 +81,7 @@ export default function Signin() {
                 type="password"
                 autoComplete="current-password"
                 required
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-slate-600 sm:text-sm sm:leading-6"
                 value={password}
                 onChange={(e) => {
                   setPassword(e.currentTarget.value);
@@ -98,18 +93,31 @@ export default function Signin() {
           <div>
             <button
               type="button"
-              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              className="flex w-full justify-center rounded-md bg-slate-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-slate-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-slate-600"
               onClick={validateInput}
             >
               Sign in
             </button>
           </div>
+
+          {error && (
+            <div className="flex h-8 items-end text-center space-x-1">
+              <>
+                <p
+                  aria-live="polite"
+                  className="text-sm text-red-500 basis-full"
+                >
+                  {error}
+                </p>
+              </>
+            </div>
+          )}
         </form>
 
         <p className="mt-10 text-center text-sm text-gray-500">
           Not a member?{" "}
           <Link
-            className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+            className="font-semibold leading-6 text-slate-600 hover:text-slate-500"
             href="/auth/signup"
           >
             Sign up
