af226ad4e9cd7b67eb7c24bb4ef372a75d551a91
aguzovatii <guzovatii.anatolii@gmail.com>
2023-10-30 19:47:46 +0200

    Move from vanilla CSS to tailwind (#74)

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>
+    <html lang="en" className="h-full">
+      <body className="h-full">
         <Providers>{children}</Providers>
       </body>
     </html>
