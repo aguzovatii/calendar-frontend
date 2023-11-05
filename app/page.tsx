7fef55bce3fbb09c609d6d5e848d181f41274aae
Anatolii Guzovatii <guzovatii.anatolii@gmail.com>
2023-11-05 19:37:14 +0200

    Add the list of habits

--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,17 @@ import CalendarPage from "./calendar/calendar-page";
 import { useSession } from "next-auth/react";
 import { redirect } from "next/navigation";
 import HabitPage from "./calendar/habit-creator";
+import HabitList from "./calendar/habit-list";
 
 export default function Page() {
-  const { data: session, status } = useSession();
+  const { status } = useSession();
 
   if (status === "authenticated") {
     return(
     <div>
       <div>
       <div>
-        
+        <HabitList />
       </div>
       <div>
         <HabitPage />
