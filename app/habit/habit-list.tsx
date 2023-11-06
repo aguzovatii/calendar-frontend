9cc8d5e7b5375bb73c733fa6c702930e90bddcc5
Diana Jacota <diana.jacota@gmail.com>
2023-11-06 15:55:00 +0200

    Style the application

--- app/habit/habit-list.tsx
+++ app/habit/habit-list.tsx
@@ -1,4 +1,5 @@
 import {ScrollArea} from "@/components/ui/scroll-area"
+import { Separator } from "@/components/ui/separator";
 
 export default function HabitList({
   habits,
@@ -7,17 +8,18 @@ export default function HabitList({
 }) {
 
   return (
-    <>
-      <h1>Habits:</h1>
-      <ScrollArea>
+    <div className="h-full">
+      <h1 className="text-xl font-bold leading-9 tracking-tight text-gray-900 ml-2 pl-0">Habits:</h1>
+      <ScrollArea className="h-100">
       {habits.map((habit) => (
           <>
-            <div key={habit.name} className="text-sm">
+            <div key={habit.name} className="border-l-2 border-slate-300 hover:border-slate-500 ml-3 pl-3 text-slate-600 cursor-pointer pt-2">
               {habit.name}
             </div>
           </>
         ))}
+
       </ScrollArea>
-    </>
+    </div>
   );
 }
