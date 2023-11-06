da82d9dc3c80feb99ab0cf1649ea572bddf60a34
Anatolii Guzovatii <guzovatii.anatolii@gmail.com>
2023-11-06 13:34:33 +0200

    Style the habits section

--- app/habit/habit-page.tsx
+++ app/habit/habit-page.tsx
@@ -10,30 +10,30 @@ const fetcher: Fetcher<Habit[], [string, string]> = ([url, token]) =>
 
 export default function HabitPage() {
 
-    const { data: session, status } = useSession();
+  const { data: session, status } = useSession();
 
-    if (status !== "authenticated") {
-      return <div>{status}</div>;
-    }
+  if (status !== "authenticated") {
+    return <div>{status}</div>;
+  }
 
-    const { data , error, isLoading, mutate } = useSWR(
-        [
-          process.env.NEXT_PUBLIC_CALENDAR_BACKEND_URL + "/habit",
-          session.accessToken,
-        ],
-        fetcher,
-      );
+  const { data , error, isLoading, mutate } = useSWR(
+    [
+      process.env.NEXT_PUBLIC_CALENDAR_BACKEND_URL + "/habit",
+      session.accessToken,
+    ],
+    fetcher,
+  );
 
-    if (error) return <div>failed to load</div>;
+  if (error) return <div>failed to load</div>;
 
-    return(
-        <>
-      <div>
+  return(
+    <div className="h-full flex flex-col">
+      <div className="basis-11/12">
         <HabitList habits={!isLoading ? data! : []}/>
       </div>
-      <div>
-        <HabitCreator onHabitCreated={() => mutate()} />
+      <div className="basis-1/12 flex flex-col-reverse">
+          <HabitCreator onHabitCreated={() => mutate()} />
       </div>
-        </>
-    )
+    </div>
+  )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -3,17 +3,21 @@ import CalendarPage from "./calendar/calendar-page";
 import { useSession } from "next-auth/react";
 import { redirect } from "next/navigation";
 import HabitPage from "./habit/habit-page";
+import { Separator } from "@/components/ui/separator";
 
 export default function Page() {
   const { status } = useSession();
 
   if (status === "authenticated") {
     return(
-    <div>
-      <div>
+    <div className="h-full flex flex-row">
+      <div className="h-full basis-1/12">
         <HabitPage />
       </div>
-      <CalendarPage />
+      <Separator orientation="vertical" />
+      <div className="h-full basis-11/12">
+        <CalendarPage />
+      </div>
     </div>
     );
   }
