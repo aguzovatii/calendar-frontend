c0e3c5b43d353c4ce7e2a1df268d3eea92ec47d9
Anatolii Guzovatii <guzovatii.anatolii@gmail.com>
2023-11-08 18:29:17 +0200

    Add a left menu

--- app/page.tsx
+++ app/page.tsx
@@ -13,19 +13,22 @@ export default function Page() {
   if (status === "authenticated") {
     return (
       <div className="h-full flex flex-row">
-        <div className="h-full basis-1/12">
-          <HabitPage
-            currentHabit={currentHabit}
-            setCurrentHabit={setCurrentHabit}
-          />
-        </div>
-        <Separator orientation="vertical" />
-        <div className="h-full basis-11/12">
-          {currentHabit !== "" ? (
-            <CalendarPage habit={currentHabit} />
-          ) : (
-            <h1>Select a habit to see the details</h1>
-          )}
+        <div className="h-full w-7 bg-slate-800"></div>
+        <div className="h-full flex flex-row flex-1">
+          <div className="h-full basis-1/12">
+            <HabitPage
+              currentHabit={currentHabit}
+              setCurrentHabit={setCurrentHabit}
+            />
+          </div>
+          <Separator orientation="vertical" className="h-full" />
+          <div className="h-full basis-11/12">
+            {currentHabit !== "" ? (
+              <CalendarPage habit={currentHabit} />
+            ) : (
+                <h1>Select a habit to see the details</h1>
+              )}
+          </div>
         </div>
       </div>
     );
