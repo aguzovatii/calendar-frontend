e8da6b08ac825355587a969a478bb47cde5c65f9
Diana Jacota <diana.jacota@gmail.com>
2023-11-08 18:52:27 +0200

    Format the file

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 "use client";
+import { LogOut } from "lucide-react";
 import CalendarPage from "./calendar/calendar-page";
 import { useSession } from "next-auth/react";
 import { redirect } from "next/navigation";
 import HabitPage from "./habit/habit-page";
 import { Separator } from "@/components/ui/separator";
 import { useState } from "react";
+import { signOut } from "next-auth/react";
 
 export default function Page() {
   const { status } = useSession();
@@ -13,7 +15,14 @@ export default function Page() {
   if (status === "authenticated") {
     return (
       <div className="h-full flex flex-row">
-        <div className="h-full w-7 bg-slate-800"></div>
+        <div className="h-full w-7 bg-slate-800 flex flex-col-reverse">
+          <LogOut
+            color="white"
+            size={16}
+            className="m-1.5 cursor-pointer"
+            onClick={() => signOut()}
+          />
+        </div>
         <div className="h-full flex flex-row flex-1">
           <div className="h-full basis-1/12">
             <HabitPage
@@ -26,8 +35,8 @@ export default function Page() {
             {currentHabit !== "" ? (
               <CalendarPage habit={currentHabit} />
             ) : (
-                <h1>Select a habit to see the details</h1>
-              )}
+              <h1>Select a habit to see the details</h1>
+            )}
           </div>
         </div>
       </div>
