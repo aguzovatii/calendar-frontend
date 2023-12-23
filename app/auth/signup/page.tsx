802c200a9fcc027f9071a5e6bfa04ae6a63c71d1
Diana Jacota <diana.jacota@gmail.com>
2023-12-23 17:40:57 +0200

    Add timezone in the signup window

--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -21,6 +21,7 @@ export default function Signin() {
   const [errorType, setErrorType] = useState<SignUpErrorTypes | null>(null);
   const [errorUsername, setErrorU] = useState("");
   const [errorPassword, setErrorP] = useState("");
+  const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
   if (sessionStatus === "loading") {
     return <div>loading</div>;
@@ -113,6 +114,28 @@ export default function Signin() {
           </div>
 
           <div>
+            <div className="flex items-center justify-between">
+              <label
+                htmlFor="timeZone"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                TimeZone
+              </label>
+            </div>
+            <div className="mt-2">
+              <input
+                id="timeZone"
+                name="timeZone"
+                type="text"
+                required
+                disabled
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-slate-600 sm:text-sm sm:leading-6"
+                value={timeZone}
+              />
+            </div>
+          </div>
+
+          <div>
             <button
               type="button"
               className="flex w-full justify-center rounded-md bg-slate-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-slate-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-slate-600"
@@ -181,6 +204,7 @@ export default function Signin() {
       redirect: false,
       username: username,
       password: password,
+      timeZone: timeZone,
     }).then((response) => {
       if (response === undefined) {
         setErrorType("default");
