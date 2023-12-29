4b04cdf140016af7a094e9b4c49b52dbc793447e
Anatolii Guzovatii <guzovatii.anatolii@gmail.com>
2023-12-29 19:34:12 +0200

    Send habit state when editing the habit

--- app/calendar/edit-habit.tsx
+++ app/calendar/edit-habit.tsx
@@ -79,6 +79,7 @@ export default function HabitEditor({
       },
       body: JSON.stringify({
         name: newHabitName,
+        state: "Pending",
       }),
     }).then((response) => {
       response.ok ? edit() : alert("The habit could not be modificated");
