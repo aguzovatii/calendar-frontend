1cedb667cc6216f070b6686e44ed4bc92d54dc70
Anatolii Guzovatii <guzovatii.anatolii@gmail.com>
2024-01-30 19:00:46 +0200

    Add sample test and update tsconfig to find custom matchers from jest-dom

--- __tests__/page.test.tsx
+++ __tests__/page.test.tsx
@@ -1,13 +1,30 @@
 /**
  * @jest-environment jsdom
  */
-import { fireEvent, render, screen } from "@testing-library/react";
-import WelcomePage from "../app/welcome/page";
+import { render, screen } from "@testing-library/react";
+import HabitPage from "../app/habit/habit-page";
+import {useSession} from "next-auth/react";
+import useSwr from "swr";
+
+jest.mock("next-auth/react");
+jest.mock("swr");
 
 it("App Router: Works with Client Components (React State)", () => {
-  render(<WelcomePage />);
-  fireEvent.click(screen.getByRole("button", {
-    name: /get started/i
-  }));
-  expect(screen.getByRole("heading")).toHaveTextContent("1");
+
+  (useSession as jest.Mock).mockReturnValue({
+    data: {
+      accessToken: "test",
+    },
+    status: "authenticated",
+  });
+
+  (useSwr as jest.Mock).mockReturnValue({
+    data: [{name: "habit1",}, {name: "habit2"}],
+    error: null, 
+    isLoading: false, 
+    mutate: () => {}, 
+  });
+
+  render(<HabitPage currentHabit="test" setCurrentHabit={() => {}} />);
+  expect(screen.getByRole("heading").textContent).toBe("Habits");
 });
