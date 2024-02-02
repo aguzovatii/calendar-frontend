8eb4a337f6c817229d12d4c71ef1beba9e5bd711
aguzovatii <guzovatii.anatolii@gmail.com>
2024-02-02 18:39:52 +0200

    Add support for writting and running tests with Jest (#190)

---
+++ __tests__/habit-page.test.tsx
@@ -0,0 +1,29 @@
+/**
+ * @jest-environment jsdom
+ */
+import { render, screen } from "@testing-library/react";
+import HabitPage from "../app/habit/habit-page";
+import { useSession } from "next-auth/react";
+import useSwr from "swr";
+
+jest.mock("next-auth/react");
+jest.mock("swr");
+
+it("HabitPage has the 'Habits' title", () => {
+  (useSession as jest.Mock).mockReturnValue({
+    data: {
+      accessToken: "test",
+    },
+    status: "authenticated",
+  });
+
+  (useSwr as jest.Mock).mockReturnValue({
+    data: [{ name: "habit1" }, { name: "habit2" }],
+    error: null,
+    isLoading: false,
+    mutate: () => {},
+  });
+
+  render(<HabitPage currentHabit="test" setCurrentHabit={() => {}} />);
+  expect(screen.getByRole("heading").textContent).toBe("Habits");
+});
