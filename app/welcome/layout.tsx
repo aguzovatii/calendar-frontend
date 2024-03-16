3d9a2cea79535be55a5886c7c5bd27fe3fc69bc1
Guzovatii_Luminita <140433911+Guzovatii-L@users.noreply.github.com>
2024-03-16 09:08:04 +0200

    Add animation(sparkles) on the welcome page (#273)
    * Add animation(sparkles) on the welcome page
    
    * Apply the suggestions
    
    * Resolve the conflicts
    
    * Delete the useless lines from tailwind

--- app/welcome/layout.tsx
+++ app/welcome/layout.tsx
@@ -1,5 +1,6 @@
 import { Providers } from "@/app/providers";
 import ThemeSwitcher from "../theme-switcher";
+import { SparklesCore } from "components/ui/sparkles";
 
 export default function RootLayout({
   children,
@@ -8,7 +9,7 @@ export default function RootLayout({
 }) {
   return (
     <Providers>
-      <div className="h-full flex flex-col">
+      <div className="h-full flex flex-col overflow-hidden">
         <div className="h-8 flex flex-row">
           <div className="basis-6">
             <h1 className="text-xl flex h-7 font-bold leading-9 tracking-tight ml-2 pl-0">
@@ -20,7 +21,17 @@ export default function RootLayout({
             <ThemeSwitcher />
           </div>
         </div>
-        <div className="grow h-48">{children}</div>
+        <div className="h-full w-full absolute overflow-hidden -z-20">
+          <SparklesCore
+            background="black"
+            minSize={0.6}
+            maxSize={1.9}
+            particleDensity={110}
+            className="w-full h-full"
+            particleColor="#14b8a6"
+          />
+        </div>
+        {children}
       </div>
     </Providers>
   );
