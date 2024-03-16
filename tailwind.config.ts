bc9a0bca7322b0f3d921e297c1fe42bcc95b5ad2
Luminita Guzovatii <luminita.guzovatii@gmail.com>
2024-03-16 09:04:41 +0200

    Delete the useless lines from tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -51,14 +51,6 @@ const config: Config = {
         sm: "calc(var(--radius) - 4px)",
       },
       keyframes: {
-        meteor: {
-          "0%": { transform: "rotate(215deg) translateX(0)", opacity: "1" },
-          "70%": { opacity: "1" },
-          "100%": {
-            transform: "rotate(215deg) translateX(-500px)",
-            opacity: "0",
-          },
-        },
         "accordion-down": {
           from: { height: "0" },
           to: { height: "var(--radix-accordion-content-height)" },
